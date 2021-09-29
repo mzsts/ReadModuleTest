b087b94657bd39589be08e7342bf0b7c6ba8c6a7
Максим Баландин <bmaxim80799@gmail.com>
2021-09-30 01:29:09 +0700

    Solving minor problems

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ReadModuleTest
         {
             string data = File.ReadAllTextAsync(@"C:\Users\Максим\Desktop\shema2_cl90.net").Result;
 
-            (List<Element> elements, List<Net> nets) = Parser.ParseDataToObjects(s);
+            (List<Element> elements, List<Net> nets) = Parser.ParseDataToObjects(data);
 
             Console.ReadKey();
         }
